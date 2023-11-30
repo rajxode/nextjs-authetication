de349f71a9f7b2eada290542be29ed2d7b8b6ad3
rajxode <rajsh1202@gmail.com>
2023-11-30 13:41:44 +0530

    signup user and toast notification

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,80 @@
+
+// function to connect to db
+import { connect } from "@/dbConfig/dbConfig";
+// user model
+import User from "@/models/User";
+// request and response
+import { NextRequest,NextResponse } from "next/server";
+// for encrypting password
+import bcryptjs from 'bcryptjs';
+
+
+// connect to database
+connect();
+
+
+// create controller for post method
+export async function POST(req:NextRequest){
+    try {
+        
+        // get user's data from request
+        const reqBody = await req.json();
+
+        console.log(reqBody);
+
+        // extract data from reqBody
+        const { username, email, password } = reqBody;
+
+        // if data is missing
+        if(!username || !email || !password){
+            return NextResponse.json({
+                message:"Provide valid data"
+            },{
+                status:401
+            });
+        }
+
+        // check whether the user already exists or not
+        const userExist = await User.findOne({email});
+
+        // if user already exists return message
+        if(userExist){
+            return NextResponse.json({
+                message:'User already exist'
+            },
+            {
+                status:400
+            });
+        }
+
+
+        // encrypt the password before saving it inside the database
+        const salt = await bcryptjs.genSalt(10);
+        // encrypting
+        const hashPassword = await bcryptjs.hash(password,salt);
+
+        // create new user inside the database with username, email and encrypted password
+        const newUser = new User({
+            username,
+            email,
+            password:hashPassword
+        });
+
+        // save the user
+        await newUser.save();
+
+        // return the resposne to the user
+        return NextResponse.json({
+            message:'User Created',
+            success:true,
+            user:newUser
+        },
+        {
+            status:201
+        });
+
+    } catch (error:any) {
+        // if error return error message
+        return NextResponse.json({error:error.message},{status:500});
+    }
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,64 +3,89 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 import Link from 'next/link';
 
 
 function SignUp() {
 
+  // route to other pages
+  const router = useRouter();
+  // data of user
   const [formData,setFormData] = React.useState({
-    name:'',
+    username:'',
     email:'',
     password:'',
   });
 
-  const handleSubmit = async(e:Event) => {
+  // handle the form submit
+  const handleSubmit = async() => {
     try {
-      e.preventDefault(); 
-    } catch (err) {
+      // make the api call for signup
+      const response = await axios.post('/api/users/signup',formData);
+      // success message
+      toast.success('New User created !!!');
+      // redirect to the login page
+      router.push('/login');
+    } catch (err : any) {
       console.log(err);
+      // show error message
+      toast.error(`Error: ${err.response.data.message}`);
     }
   }
 
   return (
     <div className='flex h-screen justify-center items-center md:p-3'>
       <div className='flex flex-col justify-between w-2/5 h-1/2 rounded shadow p-2 bg-gray-100'>
+        {/* heading  */}
         <div className='text-center h-auto text-3xl font-semibold'>
           SignUp
         </div>
         <div className='flex flex-col h-1/5'>
+          {/* username input */}
           <label>Username</label>
           <input 
             type="text" 
             placeholder='Username...'
-            value={formData.name}
+            value={formData.username}
             className='h-3/5 bg-gray-200 rounded px-2 focus:outline-none'
-            onChange={(e) => setFormData({...formData,name:e.target.value})}
+            // set the value of username
+            onChange={(e) => setFormData({...formData,username:e.target.value})}
           />
         </div>
         <div className='flex flex-col h-1/5'>
+          {/* email address of user */}
           <label>Email</label>
           <input 
             type="email"
             placeholder='Email...'
             value={formData.email}
             className='h-3/5 bg-gray-200 rounded px-2 focus:outline-none'
+            // set value of email
             onChange={(e) => setFormData({...formData,email:e.target.value})}
           />
         </div>
         <div className='flex flex-col h-1/5'>
+          {/* password of user */}
           <label>Password</label>
           <input 
             type="password"
             placeholder='Password...'
             value={formData.password}
             className='h-3/5 bg-gray-200 rounded px-2 focus:outline-none'
+            // set the password of the user
             onChange={(e) => setFormData({...formData,password:e.target.value})}
           />
         </div>
         <div className='flex flex-col h-[12%]'>
-          <button className='rounded bg-blue-500 text-white h-full font-semibold'>Submit</button>
+          {/* handle the form submission */}
+          <button className='rounded bg-blue-500 text-white h-full font-semibold'
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
         </div>
+        {/* link for login page */}
         <div className='w-full h-auto flex justify-center'>
           <Link href='/login'>Login Instead</Link>
         </div>
